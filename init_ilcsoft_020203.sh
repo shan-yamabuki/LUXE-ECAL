428e57a0601248c45e6e86e8ed4446600faf90c1
Shan Huang <shan.huang@ific.uv.es>
2024-08-12 19:23:32 +0200

    MINOR Change cvmfs environmental setup script

---
+++ init_ilcsoft_020203.sh
@@ -0,0 +1,2 @@
+#source /cvmfs/ilc.desy.de/sw/x86_64_gcc82_centos7
+source /cvmfs/ilc.desy.de/sw/x86_64_gcc82_centos7/v02-02-03/init_ilcsoft.sh
---
+++ init_key4hep_20240412.sh
@@ -0,0 +1,2 @@
+#source /cvmfs/sw.hsf.org/key4hep/setup.sh
+source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-04-12
