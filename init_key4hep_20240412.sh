3e1b938682e12c78e8009ad1a01ccc757ce21c88
Shan Huang <shan.huang@ific.uv.es>
2024-08-20 17:29:43 +0200

    Environmental variables

--- init_key4hep_20240412.sh
+++ init_key4hep_20240412.sh
@@ -1,2 +1,5 @@
 #source /cvmfs/sw.hsf.org/key4hep/setup.sh
 source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-04-12
+export LUXEECAL_REPO="/lhome/ific/h/huangsha/repos/SiWECAL"
+export MARLIN_DLL="${LUXEECAL_REPO}/PixelizationProcessor/lib/libPixelizationProcessor.so:${MARLIN_DLL}"
+
