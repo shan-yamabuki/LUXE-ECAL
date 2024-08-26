8bb0824a83de521916e0970d8ff04d515dfb377a
Shan Huang <shan.huang@ific.uv.es>
2024-08-26 19:43:02 +0200

    Miscs: Calor hit processor dll

--- init_key4hep_20240412.sh
+++ init_key4hep_20240412.sh
@@ -1,5 +1,6 @@
 #source /cvmfs/sw.hsf.org/key4hep/setup.sh
 source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-04-12
 export LUXEECAL_REPO="/lhome/ific/h/huangsha/repos/SiWECAL"
+export MARLIN_DLL="${LUXEECAL_REPO}/Playground/lib/libPlaygroundProcessor.so:${MARLIN_DLL}"
 export MARLIN_DLL="${LUXEECAL_REPO}/PixelizationProcessor/lib/libPixelizationProcessor.so:${MARLIN_DLL}"
-
+export MARLIN_DLL="${LUXEECAL_REPO}/SimCalo2CaloHit/lib/libSimCalo2CaloHit.so:${MARLIN_DLL}"
