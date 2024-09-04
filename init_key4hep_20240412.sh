03b1e7ecfa899bb910ffc24cfad62f58d83de0d6
Shan Huang <shan.huang@ific.uv.es>
2024-09-04 19:16:03 +0200

    Miscs

--- init_key4hep_20240412.sh
+++ init_key4hep_20240412.sh
@@ -1,8 +1,13 @@
 #source /cvmfs/sw.hsf.org/key4hep/setup.sh
 source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-04-12
-export LUXEECAL_REPO="/lhome/ific/h/huangsha/repos/SiWECAL"
+
+export CPATH="${HOME}/repos/luxestyle:${CPATH}"
+
+export LUXEECAL_REPO="${HOME}/repos/SiWECAL"
 export MARLIN_DLL="${LUXEECAL_REPO}/Playground/lib/libPlaygroundProcessor.so:${MARLIN_DLL}"
 export MARLIN_DLL="${LUXEECAL_REPO}/PixelizationProcessor/lib/libPixelizationProcessor.so:${MARLIN_DLL}"
 export MARLIN_DLL="${LUXEECAL_REPO}/SimCalo2CaloHit/lib/libSimCalo2CaloHit.so:${MARLIN_DLL}"
 export MARLIN_DLL="${LUXEECAL_REPO}/NNClusteringProcessor/build/lib/libNNClusteringProcessor.so:${MARLIN_DLL}"
 export MARLIN_DLL="${LUXEECAL_REPO}/Arbor/lib/libRanger.so:${MARLIN_DLL}"
+export MARLIN_DLL="${LUXEECAL_REPO}/MASProcessors/lib/libGetMIPProcessor.so:${MARLIN_DLL}"
+export MARLIN_DLL="${LUXEECAL_REPO}/MASProcessors/lib/libGetEnergyResolutionProcessor.so:${MARLIN_DLL}"
