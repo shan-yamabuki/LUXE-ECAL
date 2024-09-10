aec654cbd10fe25e2431332f679e331a8a992224
Shan Huang <shan.huang@ific.uv.es>
2024-09-10 11:47:41 +0200

    Miscs

--- generation/run_scripts/generic_condor.sh
+++ generation/run_scripts/generic_condor.sh
@@ -239,7 +239,7 @@ error                   = ${run_local}/log/errors_${condorfile}_\$(Item).log
 
 #should_transfer_files   = Yes
 #when_to_transfer_output = ON_EXIT
-+JobFlavour = "largo"
++JobFlavour = "comida"
 # options: comida=corto=2h, dia=largo=1d, puente=muylargo=4d, semana=eterno=1w
 # quicker the job, shorter the queue
 
