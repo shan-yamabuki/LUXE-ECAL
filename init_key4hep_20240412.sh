78518b1a7dbd43e5bf57b355cf2045c24fb6073c
Shan Huang <shan.huang@ific.uv.es>
2024-09-11 15:21:13 +0200

    Miscs

--- init_key4hep_20240412.sh
+++ init_key4hep_20240412.sh
@@ -3,12 +3,6 @@ source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-04-12
 
 export CPATH="${HOME}/repos/luxestyle:${CPATH}"
 
-export LUXEECAL_REPO="${HOME}/repos/SiWECAL"
-export MARLIN_DLL="${LUXEECAL_REPO}/Playground/lib/libPlaygroundProcessor.so:${MARLIN_DLL}"
-export MARLIN_DLL="${LUXEECAL_REPO}/PixelizationProcessor/lib/libPixelizationProcessor.so:${MARLIN_DLL}"
-export MARLIN_DLL="${LUXEECAL_REPO}/SimCalo2CaloHit/lib/libSimCalo2CaloHit.so:${MARLIN_DLL}"
-export MARLIN_DLL="${LUXEECAL_REPO}/NNClusteringProcessor/build/lib/libNNClusteringProcessor.so:${MARLIN_DLL}"
-export MARLIN_DLL="${LUXEECAL_REPO}/Arbor/lib/libRanger.so:${MARLIN_DLL}"
-export MARLIN_DLL="${LUXEECAL_REPO}/MASProcessors/lib/libGetMIPProcessor.so:${MARLIN_DLL}"
-export MARLIN_DLL="${LUXEECAL_REPO}/MASProcessors/lib/libGetEnergyResolutionProcessor.so:${MARLIN_DLL}"
-export MARLIN_DLL="${LUXEECAL_REPO}/ClusterAnalysis/lib/libClusterAnalysisProcessor.so:${MARLIN_DLL}"
+for dll in $( ls ${HOME}/Applications/lib/*.so ); do
+   export MARLIN_DLL="${dll}:${MARLIN_DLL}"
+done
