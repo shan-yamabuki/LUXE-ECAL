ff67e98822070a63a444ac918f3ee30402b5e1e4
Shan Huang <shan.huang@ific.uv.es>
2024-09-19 21:12:07 +0200

    Processing scripts: event merger/two photon clustering

---
+++ processing/event_merger/htcondor_run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#for i in 35 ; do
+for i in 15 35 55 75 95 115 135 150 ; do
+    echo ${i} "hMeV merging/digitising/clustering..."
+    for j in {5..9}; do
+        j2=$( expr ${j} + 5 )
+        echo -e "\tRuns" ${j}+${j2}
+        cat merge-digitisation-nnc.xml  | sed "s/ENERGY_00000/${i}00/" \
+                                        | sed "s/RUNID1_00/${j}/" \
+                                        | sed "s/RUNID2_00/${j2}/" > "steering/mdcr_${i}00_${j}+${j2}.xml"
+        cat merge-digitise-nnc.sh   | sed "s/ENERGY_00000/${i}00/" \
+                                    | sed "s/RUNID1_00/${j}/" \
+                                    | sed "s/RUNID2_00/${j2}/" > "steering/run_mdcr_${i}00_${j}+${j2}.sh"
+        chmod +x steering/run_mdcr_${i}00_${j}+${j2}.sh
+    done
+    # To submit one job with five runs
+    cat merge-digitisation-nnc.sub  | sed "s/ENERGY_00000/${i}00/" > "steering/mdcr_${i}00.sub"
+    condor_submit steering/mdcr_${i}00.sub
+done
--- processing/event_merger/merge-digitise-nnc.sh
+++ processing/event_merger/merge-digitise-nnc.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# for i in 15 ; do
-for i in 35 55 75 95 115 135 150; do
-    echo $i hMeV merging/digitising/clustering...
-    for j in {5..9}; do
-        j2=$( expr ${j} + 5 )
-        echo -e "\tRuns" ${j}+${j2}
-        cat merge-digitisation-nnc.xml  | sed "s/ENERGY_00000/${i}00/" \
-                                        | sed "s/RUNID1_00/${j}/" \
-                                        | sed "s/RUNID2_00/${j2}/" > "steering/mdcr_${i}_${j}+${j2}.xml"
-        Marlin steering/mdcr_${i}_${j}+${j2}.xml 1>logs/mdcr_${i}_${j}+${j2}.log 2>logs/mdcr_${i}_${j}+${j2}.err &
-    done
-done
+j=$1
+
+
+echo "Sourcing the following script: "
+echo -e "\t/lhome/ific/h/huangsha/.setup_key4hep"
+source /lhome/ific/h/huangsha/.setup_key4hep
+
+
+j2=$( expr ${j} + 5 )
+Marlin ${PWD}/steering/mdcr_ENERGY_00000_${j}+${j2}.xml
+
